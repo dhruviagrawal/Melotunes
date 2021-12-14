5a5cc9e177107d8a9861b6e575651f1114534fc6
anushka2422 <anushka2422saxena@gmail.com>
2021-12-14 23:07:17 +0530

    changes

--- app/src/main/java/com/example/melotunes/HomeActivity.java
+++ app/src/main/java/com/example/melotunes/HomeActivity.java
@@ -3,8 +3,10 @@ package com.example.melotunes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.content.Intent;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,5 +19,19 @@ public class HomeActivity extends AppCompatActivity {
         ll= findViewById(R.id.ll);
         b1=findViewById(R.id.b1);
         b2=findViewById(R.id.b2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomeActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomeActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
