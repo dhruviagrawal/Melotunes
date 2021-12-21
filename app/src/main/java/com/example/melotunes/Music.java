c108156fff395589d803edde5f5b5c4f6e97b618
anushka2422 <anushka2422saxena@gmail.com>
2021-12-21 17:56:22 +0530

    music

--- app/src/main/java/com/example/melotunes/Music.java
+++ app/src/main/java/com/example/melotunes/Music.java
@@ -2,13 +2,35 @@ package com.example.melotunes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Music extends AppCompatActivity {
-
+    LinearLayout musicll;
+    Button offline,online;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+        musicll=findViewById(R.id.musicll);
+        offline=findViewById(R.id.offline);
+        online=findViewById(R.id.online);
+        offline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent offlinelib=new Intent(Music.this,OfflineLibrary.class);
+                startActivity(offlinelib);
+            }
+        });
+        online.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent onlinelib=new Intent(Music.this,OnlineLibrary.class);
+                startActivity(onlinelib);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/melotunes/OnlineLibrary.java
@@ -0,0 +1,14 @@
+package com.example.melotunes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnlineLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_library);
+    }
+}
