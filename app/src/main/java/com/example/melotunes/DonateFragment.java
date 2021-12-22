1e8c5606b9ef723795efb19faff63fb00857502e
anushka2422 <anushka2422saxena@gmail.com>
2021-12-22 17:55:04 +0530

    Fragments added

---
+++ app/src/main/java/com/example/melotunes/DonateFragment.java
@@ -0,0 +1,28 @@
+package com.example.melotunes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DonateFragment extends Fragment {
+
+
+    public DonateFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_donate, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/melotunes/OfflineLibrary.java
+++ app/src/main/java/com/example/melotunes/OfflineLibrary.java
@@ -1,8 +1,19 @@
 package com.example.melotunes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.app.FragmentBreadCrumbs;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class OfflineLibrary extends AppCompatActivity {
 
@@ -10,5 +21,43 @@ public class OfflineLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_library);
+        initViewPager();
+    }
+
+    private void initViewPager() {
+        ViewPager viewpager=findViewById(R.id.viewpager);
+        TabLayout tabLayout=findViewById(R.id.tabLayout);
+        ScreenSlidePagerAdapter screenSlidePagerAdapter=new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        screenSlidePagerAdapter.addFragments(new HomeFragment(),"Home");
+        screenSlidePagerAdapter.addFragments(new PlaylistFragment(),"Playlist");
+        screenSlidePagerAdapter.addFragments(new DonateFragment(),"Donate");
+        viewpager.setAdapter(screenSlidePagerAdapter);
+        tabLayout.setupWithViewPager(viewpager);
+        
+    }
+    public static class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment>fragments;
+        private ArrayList<String>headings;
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments=new ArrayList<>();
+            this.headings=new ArrayList<>();
+
+        }
+        void addFragments(Fragment fragment,String heading)
+        {
+            fragments.add(fragment);
+            headings.add(heading);
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
